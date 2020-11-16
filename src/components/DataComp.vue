774b65a4d63a1fd9e812ffd0710983a361bfb1b2
gedgalope <gedgalope@gmail.com>
2020-11-17 02:44:43 +0800

    First Commit

---
+++ src/components/DataComp.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container fluid class="pa-0 ma-0">
+    <v-card>
+      <v-card-text class="pb-0">
+        <v-textarea v-model="agileText" solo :disabled="!edit"></v-textarea>
+      </v-card-text>
+      <v-card-actions class="pa-0">
+        <v-container fluid class="pt-0">
+          <v-row justify="center">
+            <v-col cols="4">
+              <v-btn block outlined :color="edit ? 'success' : 'primary'" @click="edit = !edit">
+                <span v-if="edit"> <v-icon class="pr-2" small>mdi-content-save</v-icon> Save</span>
+                <span v-else><v-icon class="pr-2" small>mdi-pencil</v-icon> Edit</span>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "data-comp",
+  created() {
+    if (this.values) {
+      this.agileText = this.values;
+    }
+  },
+  data() {
+    return {
+      agileText: null,
+      edit: false,
+    };
+  },
+  watch: {
+    values(val) {
+      this.agileText = val;
+    },
+  },
+  props: {
+    values: {
+      type: String,
+      default: () => "test",
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-card flat>
+          <v-card-title>
+            <v-row justify="center">Agile Values</v-row>
+          </v-card-title>
+          <v-card-text>
+            <v-row>
+              <template v-for="(values, index) in testData">
+                <v-col cols="6" :key="index">
+                  <data-comp :values="values"></data-comp>
+                </v-col>
+              </template>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-card flat>
+          <v-card-title>
+            <v-row justify="center">Agile Principles</v-row>
+          </v-card-title>
+          <v-card-text>
+            <v-row>
+              <template v-for="(values, index) in testData2">
+                <v-col cols="4" :key="index">
+                  <data-comp :values="values"></data-comp>
+                </v-col>
+              </template>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DataComp from "../components/DataComp.vue";
 
 export default {
-  name: 'home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    "data-comp": DataComp,
+  },
+  data() {
+    return {
+      testData: ["1", "2", "3", "4"],
+      testData2: ["1", "2", "3", "4",],
+    };
+  },
+};
 </script>
